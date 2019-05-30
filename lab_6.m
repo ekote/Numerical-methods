7b80e62b4dde5b09162c4588238184adab981106
Estera Kot <esterakot@gmail.com>
2019-05-30 19:46:26 +0200

    Update lab_6.m

--- lab_6.m
+++ lab_6.m
@@ -8,3 +8,21 @@ end
 function rownanie = f(x)
     rownanie = <rownanie> ;
 end
+
+
+function iter_zad_2
+  X0 = ??;
+  R(1)=X0;
+  k = 0;
+  for i=1:1:100
+    R(i+1)=<najtrudniejsze przeksztalcenie>)
+    if( abs(f(R(i+1))) < 1e-8)
+      i
+      k
+      break
+    end
+  end
+end
+function funkcja =f(x)
+  funkcja = <funkcja> ;
+end
